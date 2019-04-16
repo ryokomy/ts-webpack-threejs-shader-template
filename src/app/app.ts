d2b5cd37751957a2be14d6fb5de686e7f55e1959
Ryo Komiyama <komikomi.mikomiko@gmail.com>
2019-04-16 15:29:00 +0900

    Update to implement the following shader example https://gist.github.com/kylemcdonald/9593057

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,40 +1,93 @@
-import { Color, PerspectiveCamera, Scene, Vector3, WebGLRenderer } from 'three';
-import { Brick } from './Brick';
+import {
+  Camera,
+  Mesh,
+  PlaneGeometry,
+  Scene,
+  ShaderMaterial,
+  ShaderMaterialParameters,
+  Vector2,
+  WebGLRenderer,
+} from 'three';
 
 export class App {
-  private readonly scene = new Scene();
-  private readonly camera = new PerspectiveCamera(45, innerWidth / innerHeight, 0.1, 10000);
-  private readonly renderer = new WebGLRenderer({
-    antialias: true,
-    canvas: document.getElementById('main-canvas') as HTMLCanvasElement,
-  });
+  private readonly scene: Scene;
+  private readonly camera: Camera;
+  private shaderMaterialParams: ShaderMaterialParameters;
+  private material: ShaderMaterial;
+  private mesh: Mesh;
+  private readonly renderer: WebGLRenderer;
 
-  private brick: Brick;
+  private startTime: number;
 
   constructor() {
-    this.brick = new Brick(100, new Color('rgb(255,0,0)'));
-    this.scene.add(this.brick);
 
-    this.camera.position.set(200, 200, 200);
-    this.camera.lookAt(new Vector3(0, 0, 0));
+    this.startTime = Date.now();
 
-    this.renderer.setSize(innerWidth, innerHeight);
-    this.renderer.setClearColor(new Color('rgb(0,0,0)'));
+    // camera
+    this.camera =  new Camera();
+    this.camera.position.z = 1;
 
+    // material
+    const shaders: string[] = this.getShaders();
+    this.shaderMaterialParams = {
+      fragmentShader: shaders[1],
+      uniforms: {
+        resolution: { type: 'v2', value: new Vector2() },
+        time: { type: 'f', value: 1.0 },
+      },
+      vertexShader: shaders[0],
+    };
+    this.material = new ShaderMaterial(this.shaderMaterialParams);
+
+    // mesh
+    this.mesh = new Mesh(new PlaneGeometry(2, 2), this.material);
+
+    // scene
+    this.scene = new Scene();
+    this.scene.add(this.mesh);
+
+    // renderer
+    this.renderer = new WebGLRenderer({
+      antialias: true,
+      canvas: document.getElementById('main-canvas') as HTMLCanvasElement,
+    });
+    this.renderer.setPixelRatio(window.devicePixelRatio ? window.devicePixelRatio : 1);
+
+    // uniform
+    this.shaderMaterialParams.uniforms.resolution.value.x = window.innerWidth;
+    this.shaderMaterialParams.uniforms.resolution.value.y = window.innerHeight;
+
+    this.adjustCanvasSize();
     this.render();
   }
 
-  private adjustCanvasSize() {
-    this.renderer.setSize(innerWidth, innerHeight);
-    this.camera.aspect = innerWidth / innerHeight;
-    this.camera.updateProjectionMatrix();
-  }
-
   private render() {
+    this.update();
+
     this.renderer.render(this.scene, this.camera);
     requestAnimationFrame(() => { this.render(); });
 
     this.adjustCanvasSize();
-    this.brick.rotateY(0.03);
   }
+
+  private update() {
+    const elapsedMilliseconds = Date.now() - this.startTime;
+    const elapsedSeconds = elapsedMilliseconds / 1000.;
+    this.shaderMaterialParams.uniforms.time.value = 60. * elapsedSeconds;
+  }
+
+  private adjustCanvasSize() {
+    this.renderer.setSize(innerWidth, innerHeight);
+  }
+
+  private getShaders(): string[] {
+    const vertexShaderElement = document.getElementById('vertexShader');
+    const fragmentShaderElement = document.getElementById('fragmentShader');
+    const vertexShaderTextContent = vertexShaderElement ? vertexShaderElement.textContent : '';
+    const fragmentShaderTextContent = fragmentShaderElement ? fragmentShaderElement.textContent : '';
+    const vertexShader = vertexShaderTextContent ? vertexShaderTextContent : '';
+    const fragmentShader = fragmentShaderTextContent ? fragmentShaderTextContent : '';
+    return [vertexShader, fragmentShader];
+  }
+
 }
